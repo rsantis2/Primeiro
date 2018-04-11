90a298e382dd340cd4cdc6548bf867f7784e6f60
RODRIGO DE SANTIS <rodrigo.santis@senacsp.edu.br>
2018-04-11 08:54:22 -0300

    Declaração de variáveis

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace PrimeiroGit
         static void Main(string[] args)
         {
             
+            //Declaração das variáveis para guardar os valores que o usuário entrará
+            double valor1,valor2,resultado;
+            Console.Clear();
+            
+            Console.WriteLine("Digite um valor: ");
+            valor1 = double.Parse(Console.ReadLine());
+
+
+    
+
+
+
+
         }
     }
 }
